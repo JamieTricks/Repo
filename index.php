ce6910c51589c9bc73f476a7f8498097cde430a6
JamieTricks <64BitTricks@gmail.com>
2014-11-12 21:04:56 +0000

    Commit 1
    Commit 1

--- index.php
+++ index.php
@@ -12,10 +12,6 @@
 <div id="About">
 <a href="#">About</a>
 
-
-
-
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
