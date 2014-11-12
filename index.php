5d22e95eb5c399c87a994d4d05d52d22f601c9c9
JamieTricks <64BitTricks@gmail.com>
2014-11-12 20:44:23 +0000

    Test commit 3
    Test

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <title> Test site </title>
 
     <link rel="StyleSheet" href="styles.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
 <body>
-<h1> Test site </h1>
 
-<div id="Test 1"><ul> Test 1 <ul></div>
+<span style="display: inline-block; width: 70em; height: 10em"><span style="position: relative; display: inline-block; width: 70em; height: 10em"><i style="position: absolute;display:inline-block;width:97.75em;height:3em;background-color:#A8A8A8   ;left:0em;top:0em"></i></span></span>
+    
+<div id="About">
+<a href="#">About</a>
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
